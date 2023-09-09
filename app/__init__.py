244fd1ee16db7dcdbc7823c4ad35f24c90085c37
Debajit Adhikary <95972669+debajitadhikary@users.noreply.github.com>
2023-09-09 23:16:15 +0530

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+# app/__init__.py
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from your_flask_app.config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from your_flask_app import routes
---
+++ app/routes.py
@@ -0,0 +1,47 @@
+# routes.py
+
+from flask import render_template, url_for, flash, redirect, request
+from your_flask_app import app, db, bcrypt
+from your_flask_app.forms import RegistrationForm, LoginForm
+from your_flask_app.models import User
+
+# Home Page
+@app.route("/")
+def home():
+    return render_template('home.html', title='Home')
+
+# User Registration
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You can now log in.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+# User Login
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            # Log in the user using Flask-Login
+            flash('Login successful!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check your email and password.', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+# Sending an Emergency Signal
+@app.route("/emergency", methods=['GET', 'POST'])
+def emergency():
+    if request.method == 'POST':
+        # Add code for sending the signal to nearby responders
+        flash('Emergency signal sent. Help is on the way.', 'success')
+        return redirect(url_for('emergency'))
+    return render_template('emergency.html', title='Emergency Signal')
