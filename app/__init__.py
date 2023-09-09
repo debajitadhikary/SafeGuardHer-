64899fe3b1c7229734501859ec777374038878e2
Debajit Adhikary <95972669+debajitadhikary@users.noreply.github.com>
2023-09-10 00:46:30 +0530

    updation

--- app/__init__.py
+++ app/__init__.py
@@ -12,8 +12,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
+app.config.from_object('config.Config')  # Use the Config class from config.py
 db = SQLAlchemy(app)
+
+
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
