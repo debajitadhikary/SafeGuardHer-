22b6ff99ca746d91414b95c26e5aabacb55c6e82
Debajit Adhikary <95972669+debajitadhikary@users.noreply.github.com>
2023-09-10 00:22:07 +0530

    updation

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 # models.py
 
 from datetime import datetime
-from your_flask_app import db, login_manager
+from SAFEGUARDHER import db, login_manager
 from flask_login import UserMixin
 
 @login_manager.user_loader
