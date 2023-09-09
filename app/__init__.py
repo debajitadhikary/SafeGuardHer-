2a8438b65a218c31447f66fe59b82b43551c3e32
Debajit Adhikary <95972669+debajitadhikary@users.noreply.github.com>
2023-09-10 00:43:16 +0530

    updation

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,21 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'your_secret_key_here'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Use the appropriate database URI
+
+# from app import routes
+
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key_here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Use the appropriate database URI
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
-from app import routes
+from app import routes  # Import routes at the end to avoid circular import
