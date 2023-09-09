555793d9e391e1aa75a190dac846e31d773673bc
Debajit Adhikary <95972669+debajitadhikary@users.noreply.github.com>
2023-09-10 00:26:24 +0530

    updation

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,7 @@
-
-
-
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Use the appropriate database URI
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
-from SAFEGUARDHER import routes  # Update with your actual package name
+from app import routes
